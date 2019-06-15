144f49ce491cda14f2b9c6f3810d2ea54f1b4534
Noordeen Malango <noordinmalango@gmail.com>
2019-06-15 16:38:21 +0200

    constraint layout

--- app/src/main/java/com/example/notekeeper/MainActivity.java
+++ app/src/main/java/com/example/notekeeper/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        
+
     }
 
     @Override
